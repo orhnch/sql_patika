b0c037d02c95255f3adbef97243005edfa773231
ORHAN CAKMAK <102039580+orhnch@users.noreply.github.com>
2022-05-24 10:56:11 +0300

    odev9 was done!

---
+++ odev9.sql
@@ -0,0 +1,10 @@
+--dvdrental database
+
+SELECT city.city, country.country FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+
+SELECT customer.first_name, customer.last_name, payment.payment_id FROM customer
+INNER JOIN payment ON customer.customer_id = payment.customer_id;
+
+SELECT customer.first_name, customer.last_name, rental.rental_id FROM customer
+INNER JOIN rental ON customer.customer_id = rental.customer_id;
